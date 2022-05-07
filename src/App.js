806636ec725b0262cbb7b1f1026a31a869e068db
Logan Domingo <logankdomingo@gmail.com>
2022-05-06 23:11:41 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Tab from './components/Tab'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container">
+      <h1>Tabzzzzzzzzzzzz</h1>
+      <Tab items={[
+        {label: "Tab1", content: "Tab1 content asdfkjsadf kdsj f;lkasdjf lasdkjf jdkfajdkfj kdjka;llkjsd kj j kjfk djlkaj slkjd ;lkasdfkjasd fj al;kjdkf jl jslkadj fl;ajsdlkfj jdkasjdlfa;lddj akgriqo ghodfipvhpoqwihgh woiifj "},
+        {label: "Tab2", content: "Tab2 f;lkasdjf lasdkjf jdkfajdkfj kdjka;llkjsd kj j kjfk djlkaj slkjd ;lk f;lkasdjf lasdkjf jdkfajdkfj kdjka;llkjsd kj j kjfk djlkaj slkjd ;lk"},
+        {label: "Tab3", content: "Tab3 kfj kdjka;llkjsd kj j kjfk djlkaj slkjd ;lk f;lkasdjf lasdkjf jdkfajdkfj kdjka;llkjsd kj jkfj kdjka;llkjsd kj j kjfk djlkaj slkjd ;lk f;lkasdjf lasdkjf jdkfajdkfj kdjka;llkjsd kj j"},]}></Tab>
     </div>
   );
 }
